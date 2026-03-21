037b65553e759ba3fb2b5983083f3db600ea0c1a
Sofi <juanjo3106giraldo@gmail.com>
2026-03-21 13:44:00 -0500

    PLayer Entity Created

--- SportsLeague.Domain/Entities/Team.cs
+++ SportsLeague.Domain/Entities/Team.cs
@@ -11,5 +11,10 @@ namespace SportsLeague.Domain.Entities
         public string Stadium { get; set; } = string.Empty;
         public string? LogoUrl { get; set; }
         public DateTime FoundedDate { get; set; }
+
+
+        // Navigation Property - Colección de jugadores
+        public ICollection<Player> Players { get; set; } = new List<Player>();
+
     }
 }
