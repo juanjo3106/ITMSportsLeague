f983ec696a3390cae148c40ed0a48e43ff53aea1
Sofi <juanjo3106giraldo@gmail.com>
2026-03-28 18:17:29 -0500

    Ending Referee, team. Tournemt and TournamentTeam entities. Update the database

---
+++ SportsLeague.DataAccess/Reprositories/TournamentTeamRepository.cs
@@ -0,0 +1,32 @@
+﻿using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsLeague.DataAccess.Reprositories
+{
+    public class TournamentTeamRepository : GenericRepository<TournamentTeam>, ITournamentTeamRepository
+    {
+        public TournamentTeamRepository(LeagueDbContext context) : base(context)
+        {
+        }
+
+        public async Task<TournamentTeam?> GetByTournamentAndTeamAsync(
+            int tournamentId, int teamId)
+        {
+            return await _dbSet
+                .FirstOrDefaultAsync(tt =>
+                    tt.TournamentId == tournamentId && tt.TeamId == teamId);
+        }
+
+        public async Task<IEnumerable<TournamentTeam>> GetByTournamentAsync(
+            int tournamentId)
+        {
+            return await _dbSet
+                .Where(tt => tt.TournamentId == tournamentId)
+                .Include(tt => tt.Team)
+                .ToListAsync();
+        }
+    }
+
+}
--- SportsLeague.Domain/Entities/Team.cs
+++ SportsLeague.Domain/Entities/Team.cs
@@ -15,6 +15,7 @@ namespace SportsLeague.Domain.Entities
 
         // Navigation Property - Colección de jugadores
         public ICollection<Player> Players { get; set; } = new List<Player>();
+        public ICollection<TournamentTeam> TournamentTeams { get; set; } = new List<TournamentTeam>();
 
     }
 }
